407718058a9876189f84afad00b1120a7812a0ca
meciusMP <giedrius.mecius@mediapark.com>
2022-02-19 18:25:32 +0200

    started navigation

--- app/src/main/java/com/giedriusmecius/listings/MainActivityViewModel.kt
+++ app/src/main/java/com/giedriusmecius/listings/MainActivityViewModel.kt
@@ -1,2 +1,13 @@
 package com.giedriusmecius.listings
 
+import com.giedriusmecius.listings.utils.state.BaseViewModel
+
+class MainActivityViewModel : BaseViewModel<MainActivityState, MainActivityState.Event>(
+    MainActivityState()
+) {
+    override fun handleState(newState: MainActivityState) {
+        when (newState.request) {
+
+        }
+    }
+}
