740c2eac53a64f45fd7f185048afbaa5b3b9a8d9
meciusMP <giedrius.mecius@mediapark.com>
2022-03-03 08:14:18 +0200

    started with tab layout

---
+++ app/src/main/java/com/giedriusmecius/listings/ui/home/fragments/DemoFragment.kt
@@ -0,0 +1,34 @@
+package com.giedriusmecius.listings.ui.home.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.core.os.bundleOf
+import com.giedriusmecius.listings.databinding.FragmentDemoBinding
+import com.giedriusmecius.listings.ui.common.base.BaseFragment
+
+class DemoFragment :
+    BaseFragment<FragmentDemoBinding>(FragmentDemoBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("MANO","created, ${requireArguments().getString(TITLE)}")
+        with(binding) {
+            demoTitle.text = requireArguments().getString(TITLE)
+        }
+    }
+
+    companion object {
+        private const val TITLE = "title"
+        fun getInstance(
+            title: String
+        ): DemoFragment {
+            val instance = DemoFragment()
+            instance.arguments =
+                bundleOf(
+                    TITLE to title
+                )
+            return instance
+        }
+    }
+}
