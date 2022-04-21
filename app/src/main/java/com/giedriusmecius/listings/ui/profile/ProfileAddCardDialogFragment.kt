5de600f770bf073305d55e9b5ee5dde38ec5197b
Giederius <49857072+Giederius@users.noreply.github.com>
2022-04-21 08:08:07 +0200

    started card validation

--- app/src/main/java/com/giedriusmecius/listings/ui/profile/ProfileAddCardDialogFragment.kt
+++ app/src/main/java/com/giedriusmecius/listings/ui/profile/ProfileAddCardDialogFragment.kt
@@ -3,24 +3,25 @@ package com.giedriusmecius.listings.ui.profile
 import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import com.giedriusmecius.listings.R
 import com.giedriusmecius.listings.data.remote.model.CC
 import com.giedriusmecius.listings.databinding.DialogProfileAddCardBinding
-import com.giedriusmecius.listings.ui.profileDrawers.ProfileDrawersAdjustDialogFragment
 import com.giedriusmecius.listings.utils.extensions.setNavigationResult
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ProfileAddCardDialogFragment : BottomSheetDialogFragment() {
     private var _binding: DialogProfileAddCardBinding? = null
     private val binding get() = _binding!!
-    private val cardTypeCheck = ArrayList<String>()
+    private val cardTypeCheck = ArrayList<CardTypes>()
     private val newCard: CC? = null
 
     override fun getTheme(): Int = R.style.AppBottomSheetDialogTheme
@@ -40,13 +41,54 @@ class ProfileAddCardDialogFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        cardTypeCheck.add("^4[0-9]{6,}$")
-//        cardTypeCheck.add("^5[1-5][0-9]{5,}$")
+        cardTypeCheck.add(CardTypes.Visa)
+        cardTypeCheck.add(CardTypes.MasterCard)
 
         dialog?.window?.setSoftInputMode(4) // VISIBLE KEYBOARD
 
         with(binding) {
             addCardNumberTextEdit.apply {
+                this.addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                    override fun onTextChanged(
+                        p0: CharSequence?, p1 : Int, p2: Int, p3: Int
+                    ) {
+                        val formattedCardNumber =
+                            p0.toString().replace(" ", "").chunked(CARD_NUMBER_CHUNKS).joinToString(" ")
+                        if (formattedCardNumber != p0.toString()) {
+                            binding.addCardNumberTextEdit.apply {
+                                setText(formattedCardNumber)
+                                setSelection(formattedCardNumber.length)
+                            }
+                        }
+                    }
+
+                    override fun afterTextChanged(s: Editable?) {
+                        cardTypeCheck.forEach {
+                            for (p in cardTypeCheck) {
+                                when (p) {
+                                    CardTypes.Visa -> {
+                                        if (s.toString().matches(p.regex)) {
+                                            Log.d("MANO","matches")
+                                            binding.cardContainerFrontSide.cardTypeImg.apply {
+                                                isGone = false
+                                            }
+                                        }
+                                    }
+                                    CardTypes.MasterCard -> {
+                                        if (s.toString().matches(p.regex)) {
+                                            binding.cardContainerFrontSide.cardTypeImg.apply {
+                                                isGone = false
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                })
                 this.setOnKeyListener { v, keyCode, event ->
                     if (keyCode == KeyEvent.KEYCODE_ENTER && event?.action == KeyEvent.ACTION_DOWN) {
                         // todo handle text
@@ -183,5 +225,9 @@ class ProfileAddCardDialogFragment : BottomSheetDialogFragment() {
     companion object {
         private const val CARD_NUMBER_CHUNKS = 4
         const val RESULT_KEY = "profileAddCardResultKey"
+    }
+
+    enum class CardTypes(val regex: Regex) {
+        Visa("^4[0-9]{6,}$".toRegex()), MasterCard("^5[1-5][0-9]{5,}$".toRegex())
     }
 }
--- app/src/main/java/com/giedriusmecius/listings/utils/extensions/ViewExtensions.kt
+++ app/src/main/java/com/giedriusmecius/listings/utils/extensions/ViewExtensions.kt
@@ -1,9 +1,12 @@
 package com.giedriusmecius.listings.utils.extensions
 
 import android.content.Context
+import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.isGone
 
 fun EditText.showKeyboard() {
     requestFocus()
@@ -14,4 +17,34 @@ fun EditText.showKeyboard() {
 fun EditText.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(this.windowToken, 0)
+}
+
+fun EditText.addCardAnimationWithSetText(addTextTo: TextView, animateIn: View, label: TextView, labelTitle: Int) {
+    label.text = context.getText(labelTitle)
+    this.setOnKeyListener { v, keyCode, event ->
+        if (keyCode == KeyEvent.KEYCODE_ENTER && event?.action == KeyEvent.ACTION_DOWN) {
+            addTextTo.text = this.text
+            animateIn.apply {
+                isGone = false
+                alpha = 0F
+                translationY = 50F
+                requestFocus()
+                animate()
+                    .translationY(0F)
+                    .alpha(1F)
+                    .setDuration(700)
+                    .setListener(null)
+            }
+
+            this.animate()
+                .translationY(-50F)
+                .alpha(0F)
+                .setDuration(400)
+                .setListener(null)
+                .withEndAction { v.isGone = true }
+            true
+        } else {
+            false
+        }
+    }
 }
